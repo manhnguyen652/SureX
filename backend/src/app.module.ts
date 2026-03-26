8ba60662a165f244f6d5206f7b1491707b3f297d
manhnguyen652 <manhnguyentien653@gmail.com>
2026-03-26 14:11:10 +0700

    background backend and database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // 1. Khai báo ConfigModule
+    ConfigModule.forRoot({
+      isGlobal: true, // Để có thể dùng ở mọi nơi mà không cần import lại
+    }),
+    
+    // 2. Kết nối DB bất đồng bộ (Async)
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true, // Lưu ý: tắt khi lên Production
+      }),
+    }),
+  ],
 })
 export class AppModule {}
